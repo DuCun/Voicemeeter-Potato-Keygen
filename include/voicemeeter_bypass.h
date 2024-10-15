8a2e19a5bfc936a9e006ef460b0cad952ea582f4
Med <mederis.frambourt@gmail.com>
2024-10-15 19:06:43 +0200

    Script seems to work, sometimes bad timings but i spam so its ok

--- include/voicemeeter_bypass.h
+++ include/voicemeeter_bypass.h
@@ -7,9 +7,14 @@
 #define VOICEMEETER_BANANA_VERSION "3.1.1.3"
 #define VOICEMEETER_TIMER_STRUCT_PTR_OFFSET 0x003B7BF8 // Offset to the pointer to the struct containing the timer value relative from base address
 #define VOICEMEETER_TIMER_OFFSET 0x10D48 // Offset to the timer value relative to the timer struct
-// #define VOICEMEETER_BYPASS_DEBUG 1
+#define VOICEMEETER_BYPASS_DEBUG 1
 #define WAIT_VOICEMEETER_PROCESS_FOR 120000 // ms, max time to wait for voicemeeter to start
 #define WAIT_VOICEMEETER_PROCESS_RETRY_INTERVAL 500 // ms, interval to check for voicemeeter process 
+#define REGISTER_INFO_WINDOW_TITLE "About / Registration info..."
+#define MAX_WAIT_REGISTRATION_WINDOW_OPEN 3000 // ms, max time to wait for registration window to open
+#define MAX_WAIT_TO_CLOSE_REGISTRATION_WINDOW 3000 // ms, max time to wait for registration window to close
+#define RETRY_TIME_REGISTRATION_WINDOW 50 // ms, interval to check for registration window to close
+#define VOICEMEETER_POTATO_WINDOW_NAME "Voicemeeter Potato"
 
 // memory_helper.cpp
 DWORD findPidByName(const char *name);
@@ -20,3 +25,6 @@ void make_log(const char *format, ...);
 
 // utils.c
 unsigned int get_ms_between_filetimes(FILETIME *start, FILETIME *end);
+
+// close_window.c
+int close_window(const char *window_title);
--- srcs/main.c
+++ srcs/main.c
@@ -38,12 +38,10 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	printf("?? 1\n");
 	make_log("Voicemeeter instance found with PID: %d", voicemeeter_pid);
 
 
 	HANDLE voiceemeter_handle = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_VM_READ, 0, voicemeeter_pid);
-	printf("?? 123\n");
 	if (voiceemeter_handle == NULL)
 	{
 		printf("its null\n");
@@ -51,12 +49,10 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	printf("?? 2\n");
 
 	// pointer to the timer struct
 	void *timer_struct_ptr_address = (void *)((unsigned int)getProcessBaseAddress(voicemeeter_pid) + VOICEMEETER_TIMER_STRUCT_PTR_OFFSET);
 
-	printf("?? 3\n");
 
 	// the timer struct itself
 	void *timer_struct_address = NULL;
@@ -66,28 +62,66 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	#ifdef VOICEMEETER_BYPASS_DEBUG
-	// the timer value
-	int timer_value = 0;
 	void *timer_address = (void *)((unsigned int)timer_struct_address + VOICEMEETER_TIMER_OFFSET);
-	if (ReadProcessMemory(voiceemeter_handle, timer_address, &timer_value, sizeof(timer_value), NULL) == 0)
-	{
-		make_log("[ERROR] Failed to read timer value, aborting.");
-		return EXIT_FAILURE;
-	}
-	make_log("Timer value: %d", timer_value);
-	#endif
 
-	// now just modify it to 0
-	// TODO later find update function and call it
-	int new_timer_value = 0;
-	if (WriteProcessMemory(voiceemeter_handle, timer_address, &new_timer_value, sizeof(new_timer_value), NULL) == 0)
+	// wait for at least x seconds for window to pop, if stil not found, abort
+	HWND registration_hwnd = NULL;
+	GetSystemTimeAsFileTime(&start_filetime);
+	GetSystemTimeAsFileTime(&current_filetime);
+	while (get_ms_between_filetimes(&start_filetime, &current_filetime) < MAX_WAIT_REGISTRATION_WINDOW_OPEN)
 	{
-		make_log("[ERROR] Failed to write timer value, aborting.");
+		registration_hwnd = FindWindow(NULL, REGISTER_INFO_WINDOW_TITLE);
+		if (registration_hwnd != NULL)
+			break;
+
+		#ifdef VOICEMEETER_BYPASS_DEBUG
+		make_log("Waiting for registration window to open... (%d / %d ms elapsed)", get_ms_between_filetimes(&start_filetime, &current_filetime), MAX_WAIT_REGISTRATION_WINDOW_OPEN);
+		#endif
+
+		Sleep(RETRY_TIME_REGISTRATION_WINDOW);	
+		GetSystemTimeAsFileTime(&current_filetime);
+	}
+	if (registration_hwnd == NULL)
+	{
+		make_log("[ERROR] Registration window not found after %d ms, aborting.", MAX_WAIT_REGISTRATION_WINDOW_OPEN);
 		return EXIT_FAILURE;
 	}
-	
-	make_log("Sucessfully set the timer value to 0.");
+
+	make_log("Registration window found, try to close it.");
+
+	while (get_ms_between_filetimes(&start_filetime, &current_filetime) < MAX_WAIT_TO_CLOSE_REGISTRATION_WINDOW){
+		#ifdef VOICEMEETER_BYPASS_DEBUG
+		int timer_value = 0;
+		if (ReadProcessMemory(voiceemeter_handle, timer_address, &timer_value, sizeof(timer_value), NULL) == 0)
+			make_log("[WARNING] Failed to read timer value.");
+		else
+			make_log("Timer value: %d", timer_value);
+		#endif
+
+		// now just modify it to 0
+		// TODO later find update function and call it
+		int new_timer_value = 0;
+		if (WriteProcessMemory(voiceemeter_handle, timer_address, &new_timer_value, sizeof(new_timer_value), NULL) == 0)
+		{
+			make_log("[ERROR] Failed to write timer value, retrying.");
+			continue;
+		}
+		make_log("Sucessfully set the timer value to 0.");
+
+		int close_window_res = close_window(REGISTER_INFO_WINDOW_TITLE);
+		if (close_window_res == 0)
+			break;
+		// else if (close_window_res == 2)
+		// 	break;
+		Sleep(RETRY_TIME_REGISTRATION_WINDOW);
+		GetSystemTimeAsFileTime(&current_filetime);
+	}
+
+	// this sends close message to the window, sending it to tray if setting is enabled
+	close_window(VOICEMEETER_POTATO_WINDOW_NAME);
+
+	make_log("Voicemeeter Bypass successfully completed.");
+
 
 	return 0;
 }
